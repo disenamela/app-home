468e31c92fd9f22bb8e285ef07e26269f0db6cd7
Matias Gonzalez Fernandez <matiasngf@hotmail.com>
2021-08-15 20:49:50 -0300

    about fix

--- pages/about.js
+++ pages/about.js
@@ -5,7 +5,6 @@ export default function About() {
 		<div className="container-md my-8">
 			<h1 className="mb-4">Hola mundo!</h1>
 			<p>Somos 2 amigos, diseñadores, con una pasión enorme por nuestra profesión. Allá por 2019 empezamos con este proyecto, cuando no tuvimos mejor idea que ponernos a armar pequeñas aplicaciones que nos ayuden a nosotros y a otros diseñadores. <code>Diseñamela</code> nace para eso: queremos hacerle la vida más fácil a nuestros colegas diseñadores.</p>
-			<p>A continuación las funciones hasta ahora disponibles:</p>
 		</div>
 	)
 }
