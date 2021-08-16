da1bc31dcd82e151441b556e73c85dbfee500da9
Matias Gonzalez Fernandez <matiasngf@hotmail.com>
2021-08-15 21:07:54 -0300

    cositas

--- components/layouts/MainLayout.js
+++ components/layouts/MainLayout.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 import { useState } from "react";
 import Heading from "../../adapters/Headings";
 import GithubLogo from "../GithubLogo";
-import { PillSwitcher } from "../PillSwitcher";
+import { PillSwitcher } from "react-pill-switcher";
 import logo from '../../public/logo-disenamela.png';
 
 
@@ -28,25 +28,32 @@ export default function MainLayout({children, className}) {
 							</div>
 							<div className="col flex justify-end gap-4 center-v">
 								<Link href='/about'>
-									<a className='link'>Nosotros</a>
+									<a className='link'>
+										<i class="uil uil-user"></i>
+										Nosotros
+									</a>
 								</Link>
-								<a className='link' href='https://cafecito.app/disenamela' target='_blank'>Invitanos un cafe</a>
+								<a className='link' href='https://cafecito.app/disenamela' target='_blank'>
+									<i class="uil uil-coffee"></i>
+									Invitanos un cafe
+								</a>
 								<a href='https://github.com/disenamela' target='_blank'>
-									<GithubLogo className='github-logo'/>
+									<i class="uil uil-github" style={{ fontSize: '2em' }}></i>
 								</a>
 								<div>
 									<PillSwitcher name={'theme'} onChange={(e) => setTheme(e)}
 										options={[
 											{
 												icon: <i class="uil uil-sun" style={{ fontSize: '1.2em' }}></i>,
-												label: 'Light'
+												value:'light',
 											},
 											{
 												icon: <i class="uil uil-moon" style={{ fontSize: '1.2em' }}></i>,
-												label: 'Dark'
+												value:'dark'
 											},
 										]} />
 								</div>
+
 							</div>
 						</div>
 					</div>
--- pages/about.js
+++ pages/about.js
@@ -4,9 +4,11 @@ export default function About() {
 	return (
 		<div className="container-md my-8">
 			<h1 className="mb-4">Hola mundo!</h1>
-			<p>Somos 2 amigos, diseñadores, con una pasión enorme por nuestra profesión. Allá por 2019 empezamos con este proyecto, cuando no tuvimos mejor idea que ponernos a armar pequeñas aplicaciones que nos ayuden a nosotros y a otros diseñadores. <code>Diseñamela</code> nace para eso: queremos hacerle la vida más fácil a nuestros colegas diseñadores.</p>
+			<p>Somos 2 amigos, diseñadores y programadores, con una pasión enorme por nuestra vocación. <code>Diseñamela</code> es un proyecto Open Source y sin fines de lucro, con el objetivo de hacerle la vida más fácil a colegas diseñadores al rededor del mundo.</p>
+			<p>Allá por 2019 empezamos este proyecto como una excusa para crear algo en conjunto y seguir aprendiendo en el proceso. La propuesta se basa en desarrollar pequeñas - pero fabulosas - aplicaciones que resuelvan problemas de diseño.</p>
 		</div>
-	)
-}
-
-About.Layout = MainLayout
+		)
+	}
+	
+	About.Layout = MainLayout
+	
